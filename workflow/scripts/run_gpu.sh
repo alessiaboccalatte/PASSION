387b8e1aa13659a5d1552de7a9039b8baa87af18
alessiaboccalatte <alessia.boccalatte@inria.fr>
2024-03-14 16:25:17 +0100

    changes

---
+++ workflow/scripts/run_gpu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /scratch/clear/aboccala/anaconda3/etc/profile.d/conda.sh
+conda activate py_passion
+python /scratch/clear/aboccala/PASSION/workflow/scripts/train_section_segmentation.py
